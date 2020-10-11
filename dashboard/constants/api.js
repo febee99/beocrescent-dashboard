480945db4277b335e483b0445338b14b474583f0
Emmanuel Rayendra <emmanuelr.2018@sis.smu.edu.sg>
2020-10-11 18:01:55 +0800

    Added: api.js for fetching API constants

---
+++ dashboard/constants/api.js
@@ -0,0 +1,17 @@
+export const localBase = 'http://localhost';
+export const devBase = 'https://ui2lc3zrbc.execute-api.ap-southeast-1.amazonaws.com/dev';
+export const prodBase = '';
+
+
+// change this if required
+export const base = localBase;
+
+export const tableVision = base == localBase ? ':5000/table' : '/table';
+
+export default {
+    // base URL
+    BASE: base,
+
+    // API endpoitns
+    TABLEVISION: tableVision
+}
