f974da2cb6be04f6d03614c5f801c7b9f5ca72e7
febee99 <febee99@gmail.com>
2020-10-15 23:15:01 +0800

    Edit line chart API endpoints

--- dashboard/constants/api.js
+++ dashboard/constants/api.js
@@ -10,11 +10,14 @@ export const tableVision = base == localBase ? ':5000/tables' : '/tables';
 export const tableVisionStats = base == localBase ? ':5000/stats' : '/stats';
 export const rfidFsrVision = base == localBase ? ':5001/rfid_fsr' : '';
 
+export const rfidTrayInOut = base == localBase ? ':5001/tray_in_out' : '';
+export const rfidTrayIn = base == localBase ? ':5001/tray_in' : '';
+
 export const trayReturnVision = base == localBase ? ':5002/g6trayreturn' : '';
 export const trayReturnVision2 = base == localBase ? ':5002/g6trayreturn_s2' : '';
 
-export const rfidTrayInOut = base == localBase ? ':5001/tray_in_out' : '';
-export const rfidTrayIn = base == localBase ? ':5001/tray_in' : '';
+export const trayDistrVision = base == localBase ? ':5002/g6traydistr' : '';
+export const trayDistrVision2 = base == localBase ? ':5002/g6traydistr_s2' : '';
 
 export default {
     // base URL
@@ -28,7 +31,10 @@ export default {
     RFIDTRAYIN: rfidTrayIn,
 
     // Group 6 API endpoints
-    TRAYRETURNVISION: trayReturnVision,
-    TRAYRETURNVISION2: trayReturnVision2
+    HAICHEWRETURNVISION: trayReturnVision,
+    HAICHEWDISTRVISION: trayDistrVision,
+
+    SOONHENGRETURNVISION: trayReturnVision2,
+    SOONHENGDISTRVISION: trayDistrVision2
 
 }
