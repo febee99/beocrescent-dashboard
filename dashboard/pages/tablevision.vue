429b680ab66e7e8710e31eb040aff9cbbf578e5b
Emmanuel Rayendra <emmanuelr.2018@sis.smu.edu.sg>
2020-10-27 13:25:19 +0800

    Updated: [tablevision] display charts feature

--- dashboard/pages/tablevision.vue
+++ dashboard/pages/tablevision.vue
@@ -16,8 +16,8 @@
                         </b-datepicker>
                         </b-field>
                         <line-chart
-                        ref="nanyuanLineChart"
-                        :chartData="this.cleanerReturnInsights"
+                            ref="tablevisionChart"
+                            :chartData="this.tablevisionChartData"
                         />
                     </div>
                 </div>
@@ -55,7 +55,7 @@
                             icon="charity"
                             :number="this.selfReturnsPercentage"
                             suffix="%"
-                            label="Self-returns rate"
+                            label="Self-returns rate today"
                             description="Percentage of patrons who cleaned up their tables after eating today"
                             />
                             <card-widget
@@ -98,12 +98,17 @@ export default {
     },
 
     watch: {
-        selfReturnsPercentage: function() {
-
+        date: function() {
+            this.fetchStatsByHour()
         },
+
+        hourlyStats: function() {
+            this.$refs.tablevisionChart.renderChart(this.tablevisionChartData);
+        }
     },
 
     data() {
+        var today = new Date()
         return {
             tableVisionAPIStatus: "Offline",
             tables: [],
@@ -111,8 +116,82 @@ export default {
             selfReturnsPercentage: 0.0,
             cleanerReturnPercentage: 0.0,
             peakLazyReturns: 0,
-            totalOccupancy: 0
+            totalOccupancy: 0,
+
+            hourlyStats: [],
+
+            date: new Date(),
+            minDate: new Date(
+                today.getFullYear() - 80,
+                today.getMonth(),
+                today.getDate()
+            ),
+            maxDate: new Date(
+                today.getFullYear() + 18,
+                today.getMonth(),
+                today.getDate()
+            ),
+
+            tablevisionChartData: {
+                labels: [
+                    "06:00",
+                    "07:00",
+                    "08:00",
+                    "09:00",
+                    "10:00",
+                    "11:00",
+                    "12:00",
+                    "13:00",
+                    "14:00",
+                    "15:00",
+                    "16:00",
+                    "17:00",
+                    "18:00",
+                    "19:00"
+                ],
+                datasets: [
+                    {
+                        label: "Cleaner returns",
+                        BackgroundColor: "white",
+                        borderWidth: 3,
+                        borderColor: "#ef4250",
+                        pointBorderColor: "#249EBF",
+                        data: [],
+                    },
+                    {
+                        label: "Self returns",
+                        BackgroundColor: "white",
+                        borderWidth: 3,
+                        borderColor: "#7AD7F0",
+                        pointBorderColor: "#7AD7F0",
+                        data: [],
+                    },
+                ],
+            },
+
+            datasets: [
+                {
+                    label: "Cleaner returns",
+                    BackgroundColor: "white",
+                    borderWidth: 3,
+                    borderColor: "#ef4250",
+                    pointBorderColor: "#249EBF",
+                    data: [],
+                },
+                {
+                    label: "Self returns",
+                    BackgroundColor: "white",
+                    borderWidth: 3,
+                    borderColor: "#7AD7F0",
+                    pointBorderColor: "#7AD7F0",
+                    data: [],
+                },
+            ],
         }
+    },
+
+    mounted() {
+        this.fetchStatsByHour()
     },
 
     methods: {
@@ -126,13 +205,14 @@ export default {
                 // continue polling after that
                 this.interval = setInterval(() => {
                     this.fetchTableVacancy()
-                    this.fetchStatistics()
+                    this.fetchTotalStats()
                 }, 1000)
             }
         },
-        fetchStatistics() {
+        fetchTotalStats() {
+            var dateSelected = this.getStringFormattedDate(this.date)
             let r = this.$axios
-            .get(API.BASE + API.TABLEVISIONSTATS)
+            .get(API.BASE + API.TABLEVISIONSTATS + "/" + dateSelected.toString())
             .then((apiResponse) => {
                 var data = apiResponse.data
                 this.selfReturnsPercentage = data.self
@@ -153,6 +233,53 @@ export default {
                     }
                 }
             })
+        },
+        fetchStatsByHour() {
+            // var cleanerData = this.tablevisionChartData.datasets[0].data
+            // var selfReturnsData = this.tablevisionChartData.datasets[1].data
+            // cleaner data
+            this.tablevisionChartData.datasets[0].data = []
+            // self returns data
+            this.tablevisionChartData.datasets[1].data = []
+
+            var dateSelected = this.getStringFormattedDate(this.date)
+
+            let r = this.$axios
+            .get(API.BASE + API.TABLEVISIONSTATS + "/per_hour/" + dateSelected)
+            .then((apiResponse) => {
+                var stats = new Object(apiResponse.data)
+                this.hourlyStats = stats
+
+                for (var hour in stats) {
+                    console.log
+                    if (stats[hour].total != 0) {
+                        this.tablevisionChartData.datasets[0].data.push(stats[hour].cleaner_count)
+                        this.tablevisionChartData.datasets[1].data.push(stats[hour].self_count)
+                    }
+                }
+            })
+            .catch((error) => {
+                this.tableVisionAPIStatus = "Offline"
+                this.tables = []
+                if (error.response != undefined) {
+                    var response = error.response.data
+                    this.toastAlert(response.message, "is-danger", 5000)
+                    console.log("table vision stats hourly " + response.message)
+                } else {
+                    this.toastAlert(error, "is-danger", 5000)
+                    console.log("table vision stats hour" + error)
+                }
+            })
+        },
+        getTodayDate() {
+            function pad(s) { return (s < 10) ? '0' + s : s; }
+            var d = new Date()
+            return [pad(d.getDate()), pad(d.getMonth()+1), d.getFullYear()].join('-')
+        },
+        getStringFormattedDate(date) {
+            function pad(s) { return (s < 10) ? '0' + s : s; }
+            var d = date
+            return [pad(d.getDate()), pad(d.getMonth()+1), d.getFullYear()].join('-')
         },
         fetchTableVacancy() {
             let r = this.$axios
