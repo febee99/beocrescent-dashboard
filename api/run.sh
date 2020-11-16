e619fee1d5100a24c2a3488a62ec62a13eb315de
Emmanuel Rayendra <emmanuelr.2018@sis.smu.edu.sg>
2020-11-16 21:54:17 +0800

    Changed name to Tablevision

--- api/run.sh
+++ api/run.sh
@@ -1,7 +1,3 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 alias python=python3
-python "$DIR/app.py" &
-python "$DIR/rfid_fsr.py" &
-python "$DIR/g6trayreturn.py" &
-python "$DIR/g6overviewrate.py" & 
-python "$DIR/g6barchart.py"
+python "$DIR/application.py" &
