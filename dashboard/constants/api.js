74cb102c7b68e0f3303d478f16f84bd629e9d5d4
febee99 <febee99@gmail.com>
2020-11-16 18:00:17 +0800

    Fix conflict

--- dashboard/constants/api.js
+++ dashboard/constants/api.js
@@ -6,21 +6,21 @@ export const prodBase = '';
 // change this if required
 export const base = localBase;
 
-export const tableVision = base == localBase ? ':5000/tables' : '/tables';
-export const tableVisionStats = base == localBase ? ':5000/stats' : '/stats';
-export const rfidFsrVision = base == localBase ? ':5000/rfid_fsr' : '';
+export const tableVision = base == localBase ? ':5001/tables' : '/tables';
+export const tableVisionStats = base == localBase ? ':5001/stats' : '/stats';
+export const rfidFsrVision = base == localBase ? ':5001/rfid_fsr' : '';
 
-export const rfidTrayInOut = base == localBase ? ':5000/tray_in_out' : '';
-export const rfidTrayIn = base == localBase ? ':5000/tray_in' : '';
+export const rfidTrayInOut = base == localBase ? ':5001/tray_in_out' : '';
+export const rfidTrayIn = base == localBase ? ':5001/tray_in' : '';
 
 export const returnDistrVision = base == localBase ? ':5002/g6trayreturndistr' : '/g6trayreturndistr';
 // export const distrVision = base == localBase ? ':5002/g6traydistr' : '/g6traydistr';
 export const totalCountVision = base == localBase ? ':5002/g6total' : '/g6total';
 export const barChart = base == localBase ? ':5004/g6barchart' : '/g6barchart';
 
-export const g6Overview = base == localBase ? ':5000/g6overview' : '/g6overview';
-export const overview2 = base == localBase ? ':5000/overview2' : '/overview2';
-export const overview = base == localBase ? ':5000/overview/tray_return' : '/overview/tray_return';
+export const g6Overview = base == localBase ? ':5001/g6overview' : '/g6overview';
+export const overview2 = base == localBase ? ':5001/overview2' : '/overview2';
+export const overview = base == localBase ? ':5001/overview/tray_return' : '/overview/tray_return';
 
 export default {
     // base URL
